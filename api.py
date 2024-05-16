cef0ebc60684fe6c00f2a73dbc65e5f96a3cebb1
Youngrok Lee <cakeil@gmail.com>
2024-05-15 23:54:37 -0400

    add pin to s3 bucket

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 from vetiver import VetiverModel, VetiverAPI
 import pins
 
-b = pins.board_folder("data/model", allow_pickle_read=True)
-v = VetiverModel.from_pin(b, "penguin_model")
+b = pins.board_s3("do4ds-lab-letsgo", allow_pickle_read=True)
+v = VetiverModel.from_pin(b, "penguin_model", version="20240515T231632Z-d45a2")
 
 api = VetiverAPI(v, check_prototype=True)
 api.run(port=8080)
